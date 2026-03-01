108a439342bfac64f3b0cf6ac760cfa8bf432e02
Eduard Mashkovskii <59047757+krack89@users.noreply.github.com>
2026-03-01 23:09:31 +0700

    Update build_iso.sh

--- build_iso.sh
+++ build_iso.sh
@@ -1,7 +1,11 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
+
 echo "== ArchInstaller v0.0.7 EXTREME++ ISO Builder =="
-WORKDIR=\D:\Work
-OUTDIR="\/out"
-mkdir -p "\"
-mkarchiso -v -w /tmp/archiso-tmp -o "\" archiso
+
+WORKDIR="./work"
+OUTDIR="./out"
+
+mkdir -p "$WORKDIR" "$OUTDIR"
+
+mkarchiso -v -w "$WORKDIR" -o "$OUTDIR" archiso
